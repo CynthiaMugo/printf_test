4f64aa4f68d1e9abd625124fed50f487d90f877c
tilafu <tilafumusic@gmail.com>
2022-08-07 10:55:52 +0000

    Test

--- _printf.c
+++ _printf.c
@@ -51,3 +51,12 @@ int _printf(const char *format, ...)
 	return (len);
 }
 
+int main(void)
+{
+	int len;
+
+	len = _printf("Print a simple sentence. \n");
+	_printf("Character: [%c]\n", 'H');
+	len = _printf("Percent: [%%]\n");
+	return (0);
+}
