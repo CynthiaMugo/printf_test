d34fa1aa119fd4e9256dd26ed415176e07ca0e47
tilafu <tilafumusic@gmail.com>
2022-08-08 09:54:19 +0000

    Print oct

--- print_oct.c
+++ print_oct.c
@@ -16,7 +16,7 @@ int print_oct(va_list arguments, char *buf, unsigned int ibuf)
 	isnegative = 0;
 	if (int_input == 0)
 	{
-		ibuf = handl_buf(buf, '0', ibuf);
+		ibuf = hand1_buf(buf, '0', ibuf);
 		return (1);
 	}
 	if (int_input < 0)
@@ -35,10 +35,11 @@ int print_oct(va_list arguments, char *buf, unsigned int ibuf)
 			first_digit = 1;
 		if (first_digit)
 		{
-			ibuf = handl_buf(buf, octal[i], buf);
+			ibuf = handl_buf(buf, octal[i], ibuf);
 			count++;
 		}
-		free(binary);
-		free(octal);
-		return (count);
 	}
+	free(binary);
+	free(octal);
+	return (count);
+}
